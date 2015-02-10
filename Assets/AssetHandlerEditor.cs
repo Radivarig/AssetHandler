15b8a16597819655c63cc88e0dd0a3b0eb873ecb
Radivarig <reslav.hollos@gmail.com>
2015-02-10 19:09:21 +0100

    [chg]made it editor window

---
+++ Assets/AssetHandlerEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AssetHandlerEditor: EditorWindow {
+	float _wid = 0.2f;
+	float _hei = 0.2f;
+	string storageName = "";
+
+	public EditorAssetHandling h = new EditorAssetHandling();
+
+	[MenuItem ("Window/Asset Handler")]
+	static void Init () {
+		AssetHandlerEditor  editor = (AssetHandlerEditor)EditorWindow.GetWindow (typeof (AssetHandlerEditor));
+		editor.title = "Asset Handler";
+	}
+
+	void OnGUI(){
+		Rect mainArea = new Rect(Screen.width *0.5f *(1 -_wid), Screen.height *0.5f *(1 -_hei), Screen.width *_wid, Screen.height *_hei);
+		GUILayout.BeginArea(mainArea);
+		{
+			if(GUILayout.Button("_")){
+			}
+		}
+		GUILayout.EndArea();
+	}
+}
