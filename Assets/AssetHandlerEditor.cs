da29a5ada86664a3c6e2749ccb06aa4b9d355cb2
Radivarig <reslav.hollos@gmail.com>
2015-02-11 15:24:52 +0100

    [add]menu selection

--- Assets/AssetHandlerEditor.cs
+++ Assets/AssetHandlerEditor.cs
@@ -5,11 +5,16 @@ using System.Collections;
 public class AssetHandlerEditor: EditorWindow {
 	Rect mainArea = new Rect(0.5f, 0.5f, 0.2f, 0.2f);
 
+	Rect menuArea = new Rect(0.5f, 0.05f, 0.6f, 0.05f);
+	string[] menuItems = new string[] {"Create", "Grid 2", "Grid 3", "Grid 4"};
+	public int selMenu = 0;
+
+
 	string storageName = "storage name";
 	string storagePath = "SavedStorage";
 
 	public EditorAssetHandling h = new EditorAssetHandling();
-	
+
 	[MenuItem ("Window/Asset Handler")]
 	static void Init () {
 		AssetHandlerEditor  editor = (AssetHandlerEditor)EditorWindow.GetWindow (typeof (AssetHandlerEditor));
@@ -17,11 +22,17 @@ public class AssetHandlerEditor: EditorWindow {
 	}
 
 	void OnGUI(){
-		Rect _mainArea = new Rect(Screen.width *mainArea.x *(1 -mainArea.width), Screen.height *mainArea.y *(1 -mainArea.height), Screen.width *mainArea.width, Screen.height *mainArea.height);
+		Rect _menuArea = new Rect(Screen.width *menuArea.x *(1 -menuArea.width), Screen.height *menuArea.y *(1 -menuArea.height), Screen.width *menuArea.width, Screen.height *menuArea.height);
+		GUILayout.BeginArea(_menuArea);
+		{
+			selMenu = GUILayout.SelectionGrid(selMenu, menuItems, menuItems.Length);
+		}
+		GUILayout.EndArea();
 
+		Rect _mainArea = new Rect(Screen.width *mainArea.x *(1 -mainArea.width), Screen.height *mainArea.y *(1 -mainArea.height), Screen.width *mainArea.width, Screen.height *mainArea.height);
 		GUILayout.BeginArea(_mainArea);
 		{
-			CreateAssetGUI();
+			if (menuItems[selMenu] == "Create") CreateAssetGUI();
 		}
 		GUILayout.EndArea();
 	}
